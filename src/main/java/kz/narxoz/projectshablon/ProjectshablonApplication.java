01b6ea538bb4eadded712cb344f8221774742fa8
aigerimbakh <milashkalovec@gmail.com>
2022-11-19 15:21:13 +0300

    git init

---
+++ src/main/java/kz/narxoz/projectshablon/ProjectshablonApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.projectshablon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectshablonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectshablonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/narxoz/projectshablon/ProjectshablonApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.projectshablon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectshablonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
